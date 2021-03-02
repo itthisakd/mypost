3f456ee281e40bb7a2269247d9665f3a5511f39a
A <itthisakdr@gmail.com>
2021-03-02 14:34:21 +0700

    Added PostID into useEffect dependency

--- src/pages/SinglePostPage.js
+++ src/pages/SinglePostPage.js
@@ -5,39 +5,40 @@ import { useEffect } from "react";
 // import { BrowserRouter, Switch, useHistory, useParams } from "react-router-dom";
 // import styled from "@emotion/styled";
 import { useState } from "react";
-import { useParams } from "react-router-dom"
-import Button from "../components/Buttons.js"
+import { useParams } from "react-router-dom";
+import Button from "../components/Buttons.js";
 
 function SinglePostPage() {
-    const [post, setPost] = useState({})
-  let params = useParams() 
+  const [post, setPost] = useState({});
+  let params = useParams();
 
-    useEffect(() => {
-      async function getPostByID() {
-        let response = await fetch("http://localhost:8000/posts/" + params.postID)
-        let data = await response.json();
-        setPost(data.data)
-      }
+  useEffect(() => {
+    async function getPostByID() {
+      let response = await fetch(
+        "http://localhost:8000/posts/" + params.postID
+      );
+      let data = await response.json();
+      setPost(data.data);
+    }
     getPostByID();
-    }, [])
+  }, [params.postID]);
 
-
-    
-
-    return (
-      <div>
-        <h1
-          css={css`
-            width: 100%;
-            align-items: center;
-            padding: 15px 0 15px 15px;
-            border: 1px black solid;
-            background-color: olivedrab;
-          `}
-        >{post.title}</h1>
-        <p>{post.content}</p>
-      </div>
-    );
+  return (
+    <div>
+      <h1
+        css={css`
+          width: 100%;
+          align-items: center;
+          padding: 15px 0 15px 15px;
+          border: 1px black solid;
+          background-color: olivedrab;
+        `}
+      >
+        {post.title}
+      </h1>
+      <p>{post.content}</p>
+    </div>
+  );
 }
 
-export default SinglePostPage
+export default SinglePostPage;
